38fa7381716e91dd4ff906ed7ede69627f08cbd5
AveryApogee <dreichle@uccs.edu>
2023-01-28 01:33:45 +0000

    finished section 1

--- homework/hw-ruby-intro/lib/ruby_intro.rb
+++ homework/hw-ruby-intro/lib/ruby_intro.rb
@@ -1,4 +1,8 @@
-# When done, submit this entire file to the autograder.
+# Avery Reichler
+# 27 January 2023
+# CS 5300 
+# Homework 1, Section 1
+
 
 # Part 1
 
@@ -88,6 +92,8 @@ end
 def starts_with_consonant? s
 
   # If first character is a consonant, return true
+  # A match is considered to be:
+  # A string which begins with a non-vowel and is followed by a letter, with the whole string case-insensitive
   if s =~ /\A(?=[^aeiou])(?=[a-z])/i
     return true
   else
@@ -96,13 +102,53 @@ def starts_with_consonant? s
 end
 
 
-# TODO
+# Check if the input string in binary is a multiple of 4
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+
+  # A match is considered to be any of the following:
+  # No non-digits, zero or more digits followed by exactly one "1" followed by at least two "0"s where the sequence occurs at the end of the line
+  # Precisely the string "100"
+  # Precisely the string "0" (because zero is a multiple of every complex number)
+  if s.match(/[^\D]\d*[1]0{2,}$|^100$|^0$/)
+    return true
+  else
+    return false
+  end
 end
+
 
 # Part 3
 
+
+# BookInStock represents a book with an ISBN and a price
 class BookInStock
-# YOUR CODE HERE
+
+  # Access isbn
+  attr_reader :isbn
+
+  # Access price
+  attr_reader :price
+
+  # Mutate isbn
+  attr_writer :isbn
+
+  # Mutate price
+  attr_writer :price
+
+  # Construct a new BookInStock
+  def initialize(isbn, price)
+
+    # Check that the inputs are valid
+    if isbn == "" or price <= 0.0
+      raise ArgumentError
+    else
+      @isbn = isbn
+      @price = price
+    end
+  end
+  
+  # Return the price of the book, but as a string instead of float
+  def price_as_string
+    return "$%0.2f" % [@price]
+  end
 end
